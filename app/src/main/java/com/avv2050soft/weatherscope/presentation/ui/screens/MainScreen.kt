5d72f31217da750948e4c5b67f3680d38c576828
Anton2050 <avvsoft2050@gmail.com>
2023-08-13 11:49:26 +0300

    add location button to the MainScreen

--- app/src/main/java/com/avv2050soft/weatherscope/presentation/ui/screens/MainScreen.kt
+++ app/src/main/java/com/avv2050soft/weatherscope/presentation/ui/screens/MainScreen.kt
@@ -2,12 +2,16 @@ package com.avv2050soft.weatherscope.presentation.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -19,6 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.avv2050soft.weatherscope.presentation.bottom_navigation.BottomNavigation
@@ -29,16 +34,10 @@ import com.avv2050soft.weatherscope.presentation.bottom_navigation.NavGraph
 @Composable
 fun MainScreen(
     weatherViewModel: WeatherViewModel
-//    topBanners: List<TopBanner>,
-//    categories: List<Category>,
-//    meals: List<Meal>,
-//    onClickCategory: (String) -> Unit,
 ) {
     val navController = rememberNavController()
     Scaffold(
-        topBar = {
-            FindLocationRow()
-        },
+        topBar = { FindLocationRow() },
         bottomBar = { BottomNavigation(navController = navController) }
     ) {
         NavGraph(navHostController = navController, weatherViewModel)
@@ -47,31 +46,44 @@ fun MainScreen(
 
 @Composable
 fun FindLocationRow() {
-    Surface(
-        shape = RoundedCornerShape(8.dp),
-        shadowElevation = 8.dp,
-        color = MaterialTheme.colorScheme.surface,
+    Row(
         modifier = Modifier
-            .clickable { }
-            .padding(4.dp)
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            verticalAlignment = Alignment.Top,
-            modifier = Modifier.padding(all = 8.dp)
+        Surface(
+            shape = RoundedCornerShape(8.dp),
+            shadowElevation = 8.dp,
+            color = MaterialTheme.colorScheme.surface,
+            modifier = Modifier
+                .clickable { }
+                .padding(4.dp)
+                .weight(1f)
         ) {
-            Icon(
-                modifier = Modifier.wrapContentHeight(),
-                imageVector = Icons.Filled.Search,
-                contentDescription = null,
-            )
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(start = 8.dp),
-                text = "Krasnodar",
-                textAlign = TextAlign.Left
-            )
+            Row(
+                verticalAlignment = Alignment.Top,
+                modifier = Modifier.padding(all = 8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.wrapContentHeight(),
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = null,
+                )
+                Text(
+                    modifier = Modifier
+                        .wrapContentHeight()
+                        .padding(start = 8.dp),
+                    text = "Krasnodar",
+                    textAlign = TextAlign.Left
+                )
+            }
         }
+        Icon(
+            modifier = Modifier,
+            imageVector = Icons.Filled.LocationOn,
+            contentDescription = "Consider my location button"
+        )
     }
 }
