85e8392be9a0102df7e494709e91ea34eca18a97
Anton2050 <avvsoft2050@gmail.com>
2024-02-13 10:43:02 +0300

    change dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,10 +87,11 @@ dependencies {
     implementation ("com.github.skydoves:landscapist-glide:2.1.7")
     implementation ("io.coil-kt:coil-compose:2.4.0")
 
-    val room_version = "2.5.2"
+    val room_version = "2.6.1"
     implementation ("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
     implementation ("androidx.room:room-ktx:$room_version")
-    ksp ("androidx.room:room-compiler:$room_version")
+    kapt("androidx.room:room-compiler:$room_version")
 
     implementation("com.google.dagger:hilt-android:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
